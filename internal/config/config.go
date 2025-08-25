b9307d16c081c4a2f21e412ef9c2bd9722553a9c
sunil8777 <suniljoshi8777@gmail.com>
2025-08-25 15:28:10 +0530

    serialization of configFile

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,49 @@
 package config
 
-type Config struct{
-	
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string `yaml:"address" env-required:"true"`
+}
+
+type Config struct {
+	ENV        string `yaml:"env" env-required:"true"`
+	ServerPath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
+}
+
+func Load() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("Config is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config File not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath,&cfg); err != nil{
+		log.Fatalf("Can't read config File: %s",err.Error())
+	}
+
+	return &cfg
+
 }
