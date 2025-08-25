7e5871da3122302d366c39f44a60aa0d14cb489c
sunil8777 <suniljoshi8777@gmail.com>
2025-08-25 18:33:18 +0530

    server ready

--- cmd/prod-go-server/main.go
+++ cmd/prod-go-server/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github/sunil/prod-go-server/internal/config"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
-func main(){
-	fmt.Println("hello")
+func main() {
+	cfg := config.Load()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is home api"))
+
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started")
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed{
+			log.Fatal("failed to start server")
+		}
+	}()	
+
+	<- sigs
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil{
+		slog.Error("Failed to shutdown server",slog.String("error",err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
 }
